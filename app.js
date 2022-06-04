ae4a62280686552013747c6a32e83272723fde49
Mohtashim Ali <mohtashima85@gmail.com>
2022-06-04 09:28:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,31 +2,32 @@ let diceBtn = document.querySelector(".btn-dice"),
   id = document.querySelector("#advice-id"),
   advice = document.querySelector(".advice"),
   height = document.querySelector(".height");
-// let height = advice.clientHeight;
-// advice.style.height = `${height}px`;
+
 height.style.height = advice.clientHeight + "px";
 
 diceBtn.addEventListener("click", getAdvice);
 
 function getAdvice() {
-  // advice.style.height = `${height}px`;
   console.log("clicked");
   diceBtn.classList.add("animate-spin");
+  let audio = new Audio("./shake-roll-dice.mp3");
+  audio.play();
+  setTimeout(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        advice.innerHTML = "";
+        let slip = data.slip;
+        advice.innerHTML = `"${slip.advice}"`;
+        id.innerHTML = `ADVICE #${slip.id}`;
+        advice.style.height = "fit-content";
+        height.style.height = advice.clientHeight + "px";
 
-  fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => {
-      // advice.style.height = `0`;
-      advice.innerHTML = "";
-      let slip = data.slip;
-      advice.innerHTML = `"${slip.advice}"`;
-      id.innerHTML = `ADVICE #${slip.id}`;
-      advice.style.height = "fit-content";
-      height.style.height = advice.clientHeight + "px";
-
-      diceBtn.classList.remove("animate-spin");
-      advice.style.height = "100%";
-    });
+        diceBtn.classList.remove("animate-spin");
+        advice.style.height = "100%";
+        audio.pause();
+      });
+  }, 500);
 }
 
 window.onresize = () => {
