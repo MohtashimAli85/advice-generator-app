651cef7efa5c7e9ab92d8ae68bd5a73c782b4db2
Mohtashim Ali <mohtashima85@gmail.com>
2022-06-04 09:18:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-
-let diceBtn = document.querySelector('.btn-dice'),
-    id = document.querySelector('#advice-id'),
-    advice = document.querySelector('.advice'),
-    height = document.querySelector('.height');
+let diceBtn = document.querySelector(".btn-dice"),
+  id = document.querySelector("#advice-id"),
+  advice = document.querySelector(".advice"),
+  height = document.querySelector(".height");
 // let height = advice.clientHeight;
 // advice.style.height = `${height}px`;
-height.style.height = advice.clientHeight + 'px';
+height.style.height = advice.clientHeight + "px";
 
-diceBtn.addEventListener('click', getAdvice);
+diceBtn.addEventListener("click", getAdvice);
 
 function getAdvice() {
-    // advice.style.height = `${height}px`;
+  // advice.style.height = `${height}px`;
+  console.log("clicked");
+  diceBtn.classList.add("animate-spin");
 
-    diceBtn.classList.add('animate-spin');
-    setTimeout(() => {
-        fetch('https://api.adviceslip.com/advice')
-            .then(response => response.json())
-            .then(data => {
-                // advice.style.height = `0`;
-                advice.innerHTML = '';
-                let slip = data.slip;
-                advice.innerHTML = `"${slip.advice}"`;
-                id.innerHTML = `ADVICE #${slip.id}`;
-                advice.style.height = 'fit-content';
-                console.log(advice.clientHeight);
-                height.style.height = advice.clientHeight + 'px';
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      // advice.style.height = `0`;
+      advice.innerHTML = "";
+      let slip = data.slip;
+      advice.innerHTML = `"${slip.advice}"`;
+      id.innerHTML = `ADVICE #${slip.id}`;
+      advice.style.height = "fit-content";
+      height.style.height = advice.clientHeight + "px";
 
-
-                diceBtn.classList.remove('animate-spin');
-                advice.style.height = '100%';
-            });
-    }, 3000)
-
+      diceBtn.classList.remove("animate-spin");
+      advice.style.height = "100%";
+    });
 }
 
 window.onresize = () => {
-    advice.style.height = 'fit-content';
-    console.log(advice.clientHeight);
-    height.style.height = advice.clientHeight + 'px';
-}
+  advice.style.height = "fit-content";
+  height.style.height = advice.clientHeight + "px";
+};
